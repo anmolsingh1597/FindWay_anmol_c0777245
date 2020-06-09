1cd4bcca3770d1e48a9ec4033beda20396d632e5
Anmol Singh <anmolsingh1597@gmail.com>
2020-06-09 13:34:52 -0400

    [Anmol Singh]Current Location found

--- FindWay_anmol_c0777245/ViewController.swift
+++ FindWay_anmol_c0777245/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapObject: MKMapView!
+    let locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.requestWhenInUseAuthorization()
+        return manager
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpMapView()
     }
 
+    func setUpMapView() {
+           mapObject.showsUserLocation = true
+           mapObject.showsCompass = true
+           mapObject.showsScale = true
+           currentLocation()
+        }
+        
+        //MARK: - Helper Method
+        func currentLocation() {
+           locationManager.delegate = self
+           locationManager.desiredAccuracy = kCLLocationAccuracyBest
+           if #available(iOS 11.0, *) {
+              locationManager.showsBackgroundLocationIndicator = true
+           } else {
+              // Fallback on earlier versions
+           }
+           locationManager.startUpdatingLocation()
+        }
 
 }
-
+extension ViewController: CLLocationManagerDelegate {
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        let currentLocation = location.coordinate
+        let coordinateRegion = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 800, longitudinalMeters: 800)
+        mapObject.setRegion(coordinateRegion, animated: true)
+        locationManager.stopUpdatingLocation()
+     }
+     
+     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+     }
+}
